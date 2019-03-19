b388c67be127c2652bc0c2fd19791cc819fa23f2
Vira Bakhovska <virysja@gmail.com>
2019-03-19 15:21:22 +0200

    fix code according convention

--- Task_1/task1_file_parser.py
+++ Task_1/task1_file_parser.py
@@ -1,49 +1,56 @@
-#!/bin/python3
-import os
+"""This module parse log file:
+- print the IP address of the top downloader
+- print the least busy hour"""
+
 import sys
 from datetime import datetime
 
-#file_path = sys.argv[1]
-file_path = 'C:\PythonMentorship\Task_1\input\log_data\log_data.txt'
+FILE_PATH = sys.argv[1]
+#FILE_PATH = 'C:\PythonMentorship\Task_1\input\log_data\log_data.txt'
 
-def group_by_date_and_IP (record_list):
+def group_by_date_and_ip(record_list):
+    """This function group log data by date and ip"""
     grouped_by_date = {}
     for record in record_list:
         if record["Datetime"].date() not in grouped_by_date:
             grouped_by_date[record["Datetime"].date()] = {}
-        grouped_by_IP = grouped_by_date[record["Datetime"].date()]
-        if record["IP"] not in grouped_by_IP:
-            grouped_by_IP[record["IP"]] = record["Size"]
+        grouped_by_ip = grouped_by_date[record["Datetime"].date()]
+        if record["IP"] not in grouped_by_ip:
+            grouped_by_ip[record["IP"]] = record["Size"]
         else:
-            grouped_by_IP[record["IP"]] += record["Size"]
+            grouped_by_ip[record["IP"]] += record["Size"]
     return grouped_by_date
 
 
 def print_top_downloader(grouped_by_date):
+    """This function print IP address of the top downloader
+    (having the highest sum of download file sizes) for the given day"""
     print('IP address of the top downloader: ')
-    for date, ip in grouped_by_date.items():
-        top_downloader = max(ip, key = ip.get)
+    for date, ip_address in grouped_by_date.items():
+        top_downloader = max(ip_address, key=ip_address.get)
         print(f'{date} - {top_downloader}')
 
 
 def get_least_busy_hours(record_list):
-    list_of_hours = dict.fromkeys(range(00,24),0)
+    """This function get the hour that has the least number of requests"""
+    list_of_hours = dict.fromkeys(range(00, 24), 0)
     for record  in record_list:
         list_of_hours[record['Datetime'].hour] += 1
-    least_busy_hour = (min(list_of_hours,key = list_of_hours.get), list_of_hours[min(list_of_hours,key = list_of_hours.get)])
+    least_busy_hour = (min(list_of_hours, key=list_of_hours.get),
+                       list_of_hours[min(list_of_hours, key=list_of_hours.get)])
     return least_busy_hour
 
-with open(file_path,'r') as log_data:
+with open(FILE_PATH, 'r') as log_data:
     log_data_record = []
     for line in log_data:
-        line = line.replace('\n','')
-        str = line.split(' ')
-        log_data_record.append({'IP' : str[0],
-                                'Datetime' : datetime.strptime(str[1],'[%d/%b/%Y:%H:%M:%S]'),
-                                'Size' : int(str[2])})
+        line = line.replace('\n', '')
+        line = line.split(' ')
+        log_data_record.append({'IP' : line[0],
+                                'Datetime' : datetime.strptime(line[1], '[%d/%b/%Y:%H:%M:%S]'),
+                                'Size' : int(line[2])})
 
 
-print_top_downloader(group_by_date_and_IP(log_data_record))
+print_top_downloader(group_by_date_and_ip(log_data_record))
 
-least_busy_hour = get_least_busy_hours(log_data_record)
-print(f'The least busy hour: {least_busy_hour[0]}:00 Frequency of requests : {least_busy_hour[1]}')
+min_busy_hour = get_least_busy_hours(log_data_record)
+print(f'The least busy hour: {min_busy_hour[0]}:00 Frequency of requests : {min_busy_hour[1]}')
